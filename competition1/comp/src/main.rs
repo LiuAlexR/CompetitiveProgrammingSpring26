9a7f609e4c6d78e1df99a49d8f5c6a6ad021739d
LiuAlexR <alexliu3000@gmail.com>
2026-02-09 14:47:44 -0500

    Update

--- competition1/comp/src/main.rs
+++ competition1/comp/src/main.rs
@@ -2,23 +2,40 @@ use std::{io::Stdin, str::FromStr};
 
 fn main() {
     let mut scanner = Scanner::new();
-    println!("{}", scanner.read_i32());
-    let x: Vec<i32> = scanner.read_vec_generic();
-    for a in x {
-        println!("{a}");
-    }
-    // let x: usize = buffer.strip_suffix().parse().unwrap();
-    // for _x in 0..2 {
-    //     stdin.read_line(&mut buffer).unwrap();
-    //     let _xarr: Vec<i32> = buffer.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
-    //     buffer.clear();
-    // let x: usize = buffer.strip_suffix().parse().unwrap();
-    // }
+    p_a(&mut scanner);
 }
+fn p_a(scanner: &mut Scanner) -> () {
+    let t: i32 = scanner.read_i32();
+    let mut ans = String::new();
+    for i in 0..t {
+        let nw = scanner.read_vec_generic::<i32>();
+        let n = nw[0];
+        let w = nw[1];
+        ans.push_str(&(n - n/w).to_string());
+        ans.push('\n');
+    }
+    print!("{ans}");
+}
+fn p_b(scanner: &mut Scanner) -> () {
+    let t: i32 = scanner.read_i32();
+    let mut ans = String::new();
+    for i in 0..t {
+        let nxy = scanner.read_vec_generic::<i32>();
+        let vals = scanner.read_vec_generic::<i32>();
+        let mut max_index = 0;
+        for i in 0..vals.len() {
+            if vals[i] %
+        }
+        ans.push('\n');
+    }
+    print!("{ans}");
+}
+#[allow(dead_code)]
 struct Scanner {
     reader: Stdin,
     buffer: String,
 }
+#[allow(dead_code)]
 impl Scanner {
     pub fn new() -> Self {
         Self {
@@ -35,8 +52,19 @@ impl Scanner {
             Err(_) => panic!("Buffer error"),
         }
     }
+    pub fn read_string(&mut self) -> String {
+        self.read_line();
+        self.buffer.retain(|c| !(c == '\n' || c == '\r'));
+        return self.buffer.clone();
+    }
     pub fn read_i32 (&mut self) -> i32 {
         return self.read_number_generic::<i32>();
+    }
+    pub fn read_isize (&mut self) -> isize {
+        return self.read_number_generic::<isize>();
+    }
+    pub fn read_usize (&mut self) -> usize {
+        return self.read_number_generic::<usize>();
     }
     pub fn read_number_generic<T: FromStr> (&mut self) -> T {
         self.read_line();
---
+++ pset1/src/main.rs
@@ -0,0 +1,228 @@
+use std::{io::Stdin, str::FromStr};
+
+fn main() {
+    let mut scanner = Scanner::new();
+    problems(&mut scanner, "D");
+}
+/// The main problem solution archive
+#[allow(dead_code, unused_variables)]
+fn problems(scanner: &mut Scanner, num: &str) {
+    match num {
+        "A" => p_a(scanner),
+        "B" => p_b(scanner),
+        "C" => p_c(scanner),
+        "D" => p_d(scanner),
+        "E" => p_e(scanner),
+        "F" => p_f(scanner),
+        "G" => p_g(scanner),
+        "H" => p_h(scanner),
+        "J" => p_j(scanner),
+        _ => (),
+    }
+    /// General solution: Sort the string, while retaining a copy of the original. Then iterate through both, and add one to the solution if the characters do not match up
+    /// Time Complexity: O(n*log(n)). Sorting takes O(nlog(n)), iterating through the arrays takes O(n). I do not count turning the string to a vector of bytes, because I do not know how fast that is
+    fn p_a(scanner: &mut Scanner) -> () {
+        let num_of_cases = scanner.read_number_generic::<usize>();
+        let mut answer: String = String::new();
+        for _ in 0..num_of_cases {
+            let length_of_string = scanner.read_number_generic::<usize>();
+            let the_string = scanner.read_string();
+            let the_string_arr = the_string.as_bytes().to_vec();
+            let mut the_string_sorted = the_string_arr.clone();
+            let _ = the_string_sorted.sort();
+            let mut counter: usize = 0;
+            for i in 0..length_of_string {
+                if the_string_arr[i] != the_string_sorted[i] {
+                    counter = counter + 1;
+                }
+            }
+            answer.push_str(&counter.to_string());
+            answer.push('\n');
+        }
+        print!("{answer}");
+    }
+    /// General solution: Start at the back, move backwards. Once one pair is done, nothing done to a subsequent pair can affect that pair
+    /// Time Complexity: O(n*lg(max N))?
+    fn p_b(scanner: &mut Scanner) -> () {
+        let mut answer: String = String::new();
+        let t: u16 = scanner.read_number_generic::<u16>();
+        for _ in 0..t {
+            let n: usize = scanner.read_number_generic::<usize>();
+            
+            let mut the_vec = scanner.read_vec_generic::<i32>();
+            if n == 1 {
+                answer.push_str("0\n");
+                continue;
+            }
+            let mut solved: bool = false;
+            let mut counter: usize = 0;
+            for element in (1..(n)).rev() {
+                while the_vec[element - 1] >= the_vec[element]  && the_vec[element - 1] != 0{
+                    the_vec[element - 1] = the_vec[element - 1] / 2;
+                    counter = counter + 1;
+                }
+                if the_vec[element - 1] == the_vec[element] {
+                    answer.push_str("-1");
+                    answer.push('\n');
+                    solved = true;
+                    break;
+                }
+            }
+            if !solved {
+                answer.push_str(&counter.to_string());
+                answer.push('\n');
+            }
+        }
+        print!("{answer}");
+    }
+    /// Goal it to reduce it to 3x1 blocks because those only need one tile of paint, then add 1 for the extras.
+    /// Time Complexity: Constant time
+    fn p_c(scanner: &mut Scanner) -> () {
+        let mut answer = String::new();
+        let t: usize = scanner.read_usize();
+        for i in 0..t {
+            let the_vec = scanner.read_vec_generic::<usize>();
+            if the_vec[0] * the_vec[1] % 3 == 0 {
+                answer.push_str(&(the_vec[0] * the_vec[1] / 3).to_string());
+                answer.push('\n');
+            } else {
+                answer.push_str(&(the_vec[0] * the_vec[1] / 3 + 1).to_string());
+                answer.push('\n');
+            }
+        }
+        print!("{answer}");
+
+    }
+    /// Just choose the k that is furthest away from the current point
+    #[allow(unused_assignments)]
+    fn p_d(scanner: &mut Scanner) -> () {
+        let mut answer: String = String::new();
+        let t: usize = scanner.read_usize();
+        for _ in 0..t {
+            let nk = scanner.read_vec_generic::<usize>();
+            let n = nk[0];
+            
+            let k = nk[1];
+            let the_string = scanner.read_string().as_bytes().to_vec();
+            
+            let mut counter = 0;
+            let mut star_pos: Vec<usize> = Vec::new();
+            for i in 0..(n) {
+                if the_string[i] == 42 {
+                    star_pos.push(i);
+                }
+            }
+            if star_pos.len() == 1 {
+                answer.push_str("1\n");
+                continue;
+            }
+            let mut i = 0;
+            let mut prev_x_pos = 0;
+            counter = counter + 1;
+            while prev_x_pos < star_pos.len() - 1 {
+                i = prev_x_pos;
+                while i + 1 < star_pos.len() && star_pos[i + 1] - star_pos[prev_x_pos] <= k {
+                    i = i + 1;
+                }
+                prev_x_pos = i;
+                counter = counter + 1;
+                // if i == star_pos.len() - 1 {
+                //     prev_x_pos = i;
+                //     counter = counter + 1;
+                //     break;
+                // } else {
+                //     if star_pos[i] - star_pos[prev_x_pos] <= k {
+                //         if i + 1 == star_pos.len() - 1 && i - prev_x_pos != 1 {
+                //             counter = counter + 1;
+                //         }
+                //         continue;
+                //     } else {
+                //         i = i - 1;
+                //         prev_x_pos = i;
+                //         counter = counter + 1;
+                //         continue;
+                //     }
+                // }
+            }
+            answer.push_str(&counter.to_string());
+            answer.push('\n');
+        }
+        print!("{answer}");
+    }
+    fn p_e(scanner: &mut Scanner) -> () {
+        
+    }
+    fn p_f(scanner: &mut Scanner) -> () {
+        
+    }
+    fn p_g(scanner: &mut Scanner) -> () {
+        
+    }
+    fn p_h(scanner: &mut Scanner) -> () {
+        
+    }
+    fn p_i(scanner: &mut Scanner) -> () {
+        
+    }
+    fn p_j(scanner: &mut Scanner) -> () {
+        
+    }
+}
+/// The scanner. It allows for readings of generic numbers, strings, and vectors of numbers
+#[allow(dead_code)]
+struct Scanner {
+    reader: Stdin,
+    buffer: String,
+}
+#[allow(dead_code)]
+impl Scanner {
+    pub fn new() -> Self {
+        Self {
+            reader: std::io::stdin(),
+            buffer: String::new(),
+        }
+    }
+    fn read_line(&mut self) -> usize {
+        self.buffer.clear();
+        match self.reader.read_line(&mut self.buffer) {
+            Ok(val) => {
+                val
+            },
+            Err(_) => panic!("Buffer error"),
+        }
+    }
+    pub fn read_string(&mut self) -> String {
+        self.read_line();
+        self.buffer.retain(|c| !(c == '\n' || c == '\r'));
+        return self.buffer.clone();
+    }
+    pub fn read_i32 (&mut self) -> i32 {
+        return self.read_number_generic::<i32>();
+    }
+    pub fn read_isize (&mut self) -> isize {
+        return self.read_number_generic::<isize>();
+    }
+    pub fn read_usize (&mut self) -> usize {
+        return self.read_number_generic::<usize>();
+    }
+    pub fn read_number_generic<T: FromStr> (&mut self) -> T {
+        self.read_line();
+        self.buffer.retain(|c| !c.is_whitespace());
+        let val = self.buffer.parse::<T>();
+        match val {
+            Ok(value) => {
+                return value;
+            },
+            Err(_) => panic!("Parsing Error"),
+        }
+    }
+    pub fn read_vec_generic<T: FromStr> (&mut self) -> Vec<T> {
+        self.read_line();
+        let try_to_split = self.buffer.split_whitespace().map(|val| match val.parse::<T>() {
+            Ok(val) => val,
+            Err(_) => panic!("Parsing Error")
+        }).collect();
+        return try_to_split;
+    }
+}
+// Last Line comment marker
